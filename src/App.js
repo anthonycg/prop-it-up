46ed087a7730c173873fa2125f627c8ef41e65c1
Anthony <anthonygibson24@gmail.com>
2022-05-07 11:09:05 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import PersonCard from './components/personCard';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard 
+      firstName = {'Jane'} lastName = {'Doe'} 
+      age = {45}
+      hairColor = {'black'}/>
+      <PersonCard 
+      firstName = {'Peter'} lastName = {'Theil'} 
+      age = {42}
+      hairColor = {'green'}/>
+      <PersonCard 
+      firstName = {'Robbie'} lastName = {'Meeks'} 
+      age = {61}
+      hairColor = {'black'}/>
+      <PersonCard 
+      firstName = {'Big'} lastName = {'June'} 
+      age = {72}
+      hairColor = {'gray'}/>
+
     </div>
   );
 }
